cf001cd3133d25b8065480bd8666d9affec728f1
Admin <Admin@ADMIN-PC>
2022-01-20 21:30:33 +0300

    Добавьте файлы проекта.

---
+++ Option-one 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Option_one_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputData = Console.ReadLine();
+
+            for (int i = InputData.Length - 1; i >= 0; i--)
+
+            {
+                Console.Write(InputData[i]);
+            }
+        }
+    }
+}
